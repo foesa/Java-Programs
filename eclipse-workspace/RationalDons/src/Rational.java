7d12f2a128a16f46d6a869ac2cfda333bc0b342b
Foesa <33104551+foesa@users.noreply.github.com>
2018-02-20 13:47:17 +0000

    Rational Number base complete
    Basic part done. Need to write test code

--- eclipse-workspace/RationalDons/src/Rational.java
+++ eclipse-workspace/RationalDons/src/Rational.java
@@ -28,4 +28,69 @@ public class Rational {
 		Rational newRational = new Rational(numerator,denominator);
 		return newRational;
 	}
+	public Rational divide(Rational rationaltwo) {
+		int numerator = this.numerator*rationaltwo.denominator;
+		int denominator = this.denominator*this.numerator;
+		Rational newRational = new Rational(numerator,denominator);
+		return newRational;
+	}
+	public boolean Equals(Rational rationaltwo) {
+		int firstNumber = this.numerator*rationaltwo.denominator;
+		int secondNumber = this.denominator*rationaltwo.numerator;
+		if(firstNumber==secondNumber) {
+			return true;
+		}
+		return false;
+	}
+	public boolean isLessThan(Rational rationaltwo) {
+		int firstNumber = this.numerator*rationaltwo.denominator;
+		int secondNumber = this.denominator*rationaltwo.numerator;
+		if(secondNumber>firstNumber) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public int GCD() {
+		int gcd = 1;
+		int divisor = 1;
+		while ((divisor != -1) && (divisor < this.numerator))
+		{
+			if (this.numerator%divisor == 0)
+			{
+				gcd = divisor;
+			}
+			divisor = getNextDivisor( divisor, this.denominator );
+		}
+		return gcd;
+	}
+	public int getNextDivisor ( int lastDivisor, int number )
+	{
+
+		if ((lastDivisor > 0) && (lastDivisor < number) &&
+				(number % lastDivisor == 0))
+		{
+			int divisor = lastDivisor;
+			do {
+				divisor++;
+			} while ((divisor < number) && (number % divisor != 0));
+			if (number % divisor == 0)
+			{
+				return divisor;
+			}
+		}
+		return -1;
+	}
+	public Rational simplify() {
+		int gcd = GCD();
+		this.numerator = this.numerator/gcd;
+		this.denominator = this.denominator/gcd;
+		return this; 
+	}
+	public String toString() {
+		String rationalnumber = this.numerator+"/"+this.denominator;
+		return rationalnumber;
+	}
 }
+
