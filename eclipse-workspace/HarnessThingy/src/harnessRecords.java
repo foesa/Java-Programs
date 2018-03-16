7464d43fc380d662e51648a2046cd48ad2511d47
Foesa <33104551+foesa@users.noreply.github.com>
2018-03-16 23:02:26 +0000

    Harness records updated

--- eclipse-workspace/HarnessThingy/src/harnessRecords.java
+++ eclipse-workspace/HarnessThingy/src/harnessRecords.java
@@ -4,40 +4,42 @@ import java.util.Scanner;
 
 public class harnessRecords {
 	ArrayList<harness> harnessList = new ArrayList<harness>();
+
 	public harnessRecords() {
 	}
+
 	public harnessRecords(InputStream data) {
 		Scanner inputScanner = new Scanner(data);
 		String firstLine = inputScanner.nextLine();
 		int numberOfHarnesses = Integer.parseInt(firstLine);
 		boolean eof = false;
-		while(!eof) {
+		while (!eof) {
 			String harnessInfo = inputScanner.nextLine();
-			if(harnessInfo!=null) {
+			if (harnessInfo != null) {
 				String[] harnessString = harnessInfo.split(",");
 				String make = harnessString[0];
 				int modelNumber = Integer.parseInt(harnessString[1]);
 				int noOfUses = Integer.parseInt(harnessString[2]);
 				String nameOfLastCheck = harnessString[3];
-				harness createHarness = new harness(make,modelNumber,noOfUses,nameOfLastCheck);
+				harness createHarness = new harness(make, modelNumber, noOfUses, nameOfLastCheck);
 				this.harnessList.add(createHarness);
-			}
-			else {
+			} else {
 				eof = true;
 			}
 		}
 	}
+
 	public boolean isEmpty() {
-		if(this.harnessList.size() == 0) {
+		if (this.harnessList.size() == 0) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public void addHarness(harness addedHarness) {
 		harnessList.add(addedHarness);
 	}
-	
+
 	public harness findHarness(String make, int modelNumber) {
 		for(int count = 0; count<this.harnessList.size();count++) {
 			if(harnessList.get(count).make.equals(make) && harnessList.get(count).modelNumber == modelNumber) {
@@ -45,6 +47,10 @@ public class harnessRecords {
 			}
 		}
 		return null;
+	}
+
+	public void checkHarness(String instructorName, String harnessmake, int modelNumber ) {
+		harness checkedHarness = findHarness(harnessmake, modelNumber);
 	}
 
 }
