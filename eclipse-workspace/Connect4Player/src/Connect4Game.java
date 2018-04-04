a7d7c0c1b66907765386d34ca0c6e659359ba310
Foesa <33104551+foesa@users.noreply.github.com>
2018-04-04 09:31:20 +0100

    not working version

--- eclipse-workspace/Connect4Player/src/Connect4Game.java
+++ eclipse-workspace/Connect4Player/src/Connect4Game.java
@@ -1,3 +1,34 @@
+/* SELF ASSESSMENT
+
+Connect4Game class (35 marks)
+My class creates references to the Connect 4 Grid and two Connect 4 Players. It asks the user whether he/she would like to play/quit inside a loop. If the user decides to play then: 1. Connect4Grid2DArray is created using the Connect4Grid interface, 2. the two players are initialised - must specify the type to be ConnectPlayer, and 3. the game starts. In the game, I ask the user where he/she would like to drop the piece. I perform checks by calling methods in the Connect4Grid interface. Finally a check is performed to determine a win. 
+Comment:
+
+Connect4Grid interface (10 marks)
+I define all 7 methods within this interface.
+Comment:
+
+Connect4Grid2DArray class (25 marks) 
+My class implements the Connect4Grid interface. It creates a grid using a 2D array Implementation of the method to check whether the column to drop the piece is valid. It provides as implementation of the method to check whether the column to drop the piece is full. It provides as implementation of the method to drop the piece.  It provides as implementation of the method to check whether there is a win.
+Comment:
+
+ConnectPlayer abstract class (10 marks)
+My class provides at lest one non-abstract method and at least one abstract method. 
+Comment:
+
+C4HumanPlayer class (10 marks)
+My class extends the ConnectPlayer claas and overrides the abstract method(s). It provides the Human player functionality.
+Comment:
+
+C4RandomAIPlayer class (10 marks)
+My class extends the ConnectPlayer claas and overrides the abstract method(s). It provides AI player functionality. 
+Comment:
+
+Total Marks out of 100:
+
+*/
+
+
 import java.util.Scanner;
 
 public class Connect4Game {
